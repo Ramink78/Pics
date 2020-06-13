46841dcc5b4d121bdccb7f22c67c0c13f373bbeb
Ramin Karimi nejad <Raminkarimi1378@gmail.com>
2020-06-13 15:17:56 +0430

    Clean Project

--- app/src/main/java/com/unsplash/retrofit/MainActivity.kt
+++ app/src/main/java/com/unsplash/retrofit/MainActivity.kt
@@ -10,44 +10,12 @@ import androidx.navigation.ui.setupWithNavController
 
 
 class MainActivity : AppCompatActivity() {
-    val API_KEY="Ov-NmVnr6uWRVKNSOFm4BWIlHIwr_LZH7bW5dzOmdU0"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
         val navView: BottomNavigationView = findViewById(R.id.nav)
-
         val navController = findNavController(R.id.host)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         navView.setupWithNavController(navController)
-       /*   val request=ServiceBuilder.buildService(API::class.java)
-      val call=request.getContent(API_KEY)
-      call.enqueue(object : Callback<Data> {
-           override fun onResponse(call: Call<Data>, response: Response<Data>) {
-              if (response.isSuccessful){
-
-                  val result=response.body()
-
-                  if (result != null) {
-                      for (i in 0 until result.size){
-                          Log.i("RESPONSE", "url is: "+result[i].urls.regular)
-                      }
-                  }
-
-
-
-
-
-                  Toast.makeText(this@MainActivity,"OK",Toast.LENGTH_SHORT).show()
-
-
-              }
-          }
-           override fun onFailure(call: Call<Data>, t: Throwable) {
-              Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-               Log.i("RESPONSE",t.message)
-          }
-      })*/
 
 
 
